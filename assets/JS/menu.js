9e1afafd788f981010550dce83c3f9dc5f5f48e3
diego alvarez <diego.stonerough@gmail.com>
2021-06-06 15:26:20 -0500

    se removio boton para cerrar menu, no funciona

--- assets/JS/menu.js
+++ assets/JS/menu.js
@@ -6,16 +6,10 @@ let menuStatus = false;
 responsiveMenuIcon.addEventListener("click", function(){
     if (menuStatus === false) {
         hiddenMenu.classList.add("class", "pop-up-menu");
-        responsiveMenuIcon.classList.add("class", "open-pop-up-menu")
         closeIcon.classList.add("class", "close-pop-up-menu")
         menuStatus = true;
-    }
-})
-
-closeIcon.addEventListener("click", function() {
-    if (menuStatus === true) {
+    } else {
         hiddenMenu.classList.remove("class", "pop-up-menu"); 
-        responsiveMenuIcon.classList.remove("class", "open-pop-up-menu")
         closeIcon.classList.remove("class", "close-pop-up-menu")
         menuStatus = false;
     }
