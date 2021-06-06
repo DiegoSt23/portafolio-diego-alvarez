1450561340d2ce2172c4aea358f9f908ea699b3f
diego alvarez <diego.stonerough@gmail.com>
2021-06-06 15:20:23 -0500

    Se añadio icono para cerrar menu responsive

--- assets/JS/menu.js
+++ assets/JS/menu.js
@@ -1,13 +1,22 @@
 let responsiveMenuIcon = document.getElementById("responsive-menu-icon");
+let closeIcon = document.getElementById("close-menu-icon");
 let hiddenMenu = document.getElementById("hidden-menu");
 let menuStatus = false;
 
 responsiveMenuIcon.addEventListener("click", function(){
     if (menuStatus === false) {
         hiddenMenu.classList.add("class", "pop-up-menu");
+        responsiveMenuIcon.classList.add("class", "open-pop-up-menu")
+        closeIcon.classList.add("class", "close-pop-up-menu")
         menuStatus = true;
-    } else {
+    }
+})
+
+closeIcon.addEventListener("click", function() {
+    if (menuStatus === true) {
         hiddenMenu.classList.remove("class", "pop-up-menu"); 
+        responsiveMenuIcon.classList.remove("class", "open-pop-up-menu")
+        closeIcon.classList.remove("class", "close-pop-up-menu")
         menuStatus = false;
     }
 })
