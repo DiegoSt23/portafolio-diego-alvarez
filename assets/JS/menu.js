a8b269a4e80b9661d98d6b99b424423d394a4265
diego alvarez <diego.stonerough@gmail.com>
2021-06-06 16:21:34 -0500

    correccion de fallo en boton responside menu

--- assets/JS/menu.js
+++ assets/JS/menu.js
@@ -1,16 +1,13 @@
 let responsiveMenuIcon = document.getElementById("responsive-menu-icon");
-let closeIcon = document.getElementById("close-menu-icon");
 let hiddenMenu = document.getElementById("hidden-menu");
 let menuStatus = false;
 
 responsiveMenuIcon.addEventListener("click", function(){
     if (menuStatus === false) {
         hiddenMenu.classList.add("class", "pop-up-menu");
-        closeIcon.classList.add("class", "close-pop-up-menu")
         menuStatus = true;
     } else {
         hiddenMenu.classList.remove("class", "pop-up-menu"); 
-        closeIcon.classList.remove("class", "close-pop-up-menu")
         menuStatus = false;
     }
 })
